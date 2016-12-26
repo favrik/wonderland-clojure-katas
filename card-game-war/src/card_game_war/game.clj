b100ba6154ac1992ae799b97326f8d7c54058db8
Favio Manriquez Leon <favio@favrik.com>
2016-12-26 01:11:00 -0800

    Add first iteration for card game war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,26 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn rank-value [card]
+  (.indexOf ranks (nth card 1)))
 
-(defn play-game [player1-cards player2-cards])
+(defn suit-value [card]
+  (.indexOf suits (nth card 0)))
+
+(defn play-round [player1-card player2-card]
+  (if (= (rank-value player1-card) (rank-value player2-card))
+    (compare (suit-value player1-card) (suit-value player2-card))
+    (compare (rank-value player1-card) (rank-value player2-card))))
+
+(defn real-play [[p1cards p2cards]]
+  (let [p1 (first p1cards) p2 (first p2cards)]
+    (if (= 1 (play-round p1 p2))
+      [(conj (drop 1 p1cards) p1 p2) (drop 1 p2cards)]
+      [(drop 1 p1cards) (conj (drop 1 p2cards) p1 p2)])))
+
+(defn play-game [player1-cards player2-cards]
+  (loop [leset [player1-cards player2-cards]]
+    (cond
+      (empty? (first leset)) -1
+      (empty? (second leset)) 1
+      :else (recur (real-play leset)))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -6,14 +6,22 @@
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= 1 (play-round [:spade 5] [:spade 2]))))
+  (testing "queens are higher rank than jacks"
+    (is (= 1 (play-round [:spade :queen] [:spade :jack]))))
+  (testing "kings are higher rank than queens"
+    (is (= 1 (play-round [:spade :king] [:spade :queen]))))
+  (testing "aces are higher rank than kings"
+    (is (= 1 (play-round [:spade :ace] [:spade :king]))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= 1 (play-round [:club :ace] [:spade :ace]))))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= 1 (play-round [:diamond :ace] [:club :ace]))))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= 1 (play-round [:heart :ace] [:diamond :ace])))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (is (= 1 (play-game [[:heart :ace]] [[:spade :jack] [:club :jack]])))
+    (is (= -1 (play-game [[:spade :jack]] [[:club :jack]])))))
 
